f54dbddbdcf8fb036ad6ff5e091f6fbf3807f1c9
Nick Cain <nicain.seattle@gmail.com>
2022-01-12 20:46:19 +0000

    adding build_docker.sh script for building docker image

---
+++ community-content/alphafold_on_workbench/build_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+PROJECT=peak-responder-337818
+REPOSITORY=alphafold-demo
+LOCAL_IMAGE=alphafold-on-gcp
+REMOTE_IMAGE=${LOCAL_IMAGE?}
+TAG=latest
+REGISTRY="us-west1-docker.pkg.dev/${PROJECT?}/${REPOSITORY?}/${REMOTE_IMAGE?}:${TAG?}"
+
+git clone https://github.com/deepmind/alphafold.git
+
+cp Dockerfile alphafold/docker/Dockerfile
+cp AlphaFold.ipynb alphafold/notebooks/AlphaFold.ipynb
+
+cd alphafold && sudo docker build --tag ${LOCAL_IMAGE?}:${TAG?} -f docker/Dockerfile .
+
+sudo docker tag ${LOCAL_IMAGE?}:${TAG?} ${REGISTRY?}
+sudo docker push ${REGISTRY?}
