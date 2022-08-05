af21933e802b75531b8b4fe9efaac01b69cf0186
Soheila Zangeneh <soheilaz@google.com>
2022-08-05 16:52:59 -0400

    Inject python version to yaml file

--- .cloud-build/execute_notebook_remote.py
+++ .cloud-build/execute_notebook_remote.py
@@ -40,6 +40,7 @@ def execute_notebook_remote(
     private_pool_region: Optional[str],
     tag: Optional[str],
     timeout_in_seconds: Optional[int] = None,
+    python_version: Optional[str] = None
 ) -> operation.Operation:
     """Create and execute a single notebook on Google Cloud Build"""
     # Load build steps from YAML
@@ -51,6 +52,9 @@ def execute_notebook_remote(
         "_NOTEBOOK_GCS_URI": notebook_uri,
         "_NOTEBOOK_OUTPUT_GCS_URI": notebook_output_uri,
     }
+
+    if python_version is not None:
+      substitutions["_PYTHON_VERION"] = python_version
 
     build = cloudbuild_v1.Build()
 
