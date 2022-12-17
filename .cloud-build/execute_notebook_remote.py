5ff5ccba9179132a4b9f6119501b8eaa5289b486
Andrew Ferlitsch <aferlitsch@google.com>
2022-12-17 00:34:53 +0000

    debug: explicit pass timeout

--- .cloud-build/execute_notebook_remote.py
+++ .cloud-build/execute_notebook_remote.py
@@ -46,7 +46,6 @@ def execute_notebook_remote(
     # Load build steps from YAML
 
     print(f"DEBUG TIMEOUT {timeout_in_seconds}\n")
-    timeout_in_seconds = 700
     
 
     cloudbuild_config = yaml.load(open(CLOUD_BUILD_FILEPATH), Loader=FullLoader)
