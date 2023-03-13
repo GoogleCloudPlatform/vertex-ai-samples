d54a1d1197fc89ab25c4cc32715bc65cfa198110
Andrew Ferlitsch <aferlitsch@google.com>
2023-03-13 22:23:52 +0000

    fix: review comments

--- resource_reaper.py
+++ resource_reaper.py
@@ -1,18 +1,27 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 '''
 Automated Vertex AI resource cleanup for python-docs-samples-test
 '''
-
-# ! pip3 install google-cloud-aiplatform google-cloud-bigquery --quiet
 
 from google.cloud import aiplatform
 from google.cloud import bigquery
 import time
 import datetime
 
-PROJECT_ID = "python-docs-samples-tests"  # @param {type:"string"}
-
-# Set the project id
-# ! gcloud config set project {PROJECT_ID}
+PROJECT_ID = "python-docs-samples-tests"
 
 REGION = "us-central1"
 aiplatform.init(project=PROJECT_ID, location=REGION)
