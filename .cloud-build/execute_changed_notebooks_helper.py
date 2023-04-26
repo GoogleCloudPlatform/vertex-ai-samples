018865c59baca3d81bf36622234974174e209870
Andrew Ferlitsch <aferlitsch@google.com>
2023-04-26 19:48:48 +0000

    debug: bucket issue

--- .cloud-build/execute_changed_notebooks_helper.py
+++ .cloud-build/execute_changed_notebooks_helper.py
@@ -346,7 +346,7 @@ def _save_results(results: List[NotebookExecutionResult],
                   results_file: str):
 
     artifacts_bucket = artifacts_bucket.replace("gs://", "")
-    artifacts_bucket = "cloud-samples-data"
+    #artifacts_bucket = "cloud-samples-data"
 
     # read in existing prior results data
     rows = []
