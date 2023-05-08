10d9bd0b4129bb8a585d93d3f625f7cdff0bb92b
Andrew Ferlitsch <aferlitsch@google.com>
2023-05-08 21:58:59 +0000

    fix: selection algo

--- .cloud-build/execute_changed_notebooks_cli.py
+++ .cloud-build/execute_changed_notebooks_cli.py
@@ -139,12 +139,12 @@ changed_notebooks = execute_changed_notebooks_helper.get_changed_notebooks(
 results_bucket = f"{args.artifacts_bucket}"
 results_file = f"build_results/{args.build_id}.json"
 
+print(f"PERCENT {args.percent}")
+args.test_percent = 90
 if args.test_percent == 100:
-    print("NO SELECT")
     notebooks = changed_notebooks
     accumulative_results = {}
 else:
-    print("DO SELECTION")
     accumulative_results = execute_changed_notebooks_helper.load_results(results_bucket, results_file)
 
     notebooks = [changed_notebook for changed_notebook in changed_notebooks if execute_changed_notebooks_helper.select_notebook(changed_notebook, accumulative_results, args.test_percent)]
