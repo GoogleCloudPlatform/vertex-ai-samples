93087894eb60b4c7e29a226f8faafcf594d6e20d
Andrew Ferlitsch <aferlitsch@google.com>
2023-05-09 01:06:37 +0000

    fix: selection algo

--- .cloud-build/execute_changed_notebooks_helper.py
+++ .cloud-build/execute_changed_notebooks_helper.py
@@ -96,11 +96,10 @@ def load_results(results_bucket: str,
     accumulative_results = {}
     try:
         client = storage.Client()
-        client.bucket(results_bucket)
+        bucket = client.bucket(results_bucket)
 
         build_results_dir = os.path.dirname(results_file)
-        print("BUILD RESULTS DIR", build_results_dir)
-        blobs = client.list_blobs(build_results_dir)
+        blobs = bucket.list_blobs(build_results_dir)
         for blob in blobs:
             content = util.download_blob_into_memory(results_bucket, results_file, download_as_text=True)
             accumulative_results = {**accumulative_results, **json.loads(content)}
