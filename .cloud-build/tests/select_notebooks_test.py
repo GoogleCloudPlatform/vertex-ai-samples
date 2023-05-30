0b8453f4bfdeb9314e9fc53da8e05a0105931067
Andrew Ferlitsch <aferlitsch@google.com>
2023-05-30 22:02:12 +0000

    test: add test for select_notebook

--- .cloud-build/tests/select_notebooks_test.py
+++ .cloud-build/tests/select_notebooks_test.py
@@ -9,3 +9,13 @@ bucket_file: str = "build_results"
 accum = load_results(bucket, bucket_file)
 
 print(accum)
+
+n_select = 0
+n_notselect = 0
+for notebook in accum:
+    if select_notebook(notebook, accum, 20):
+        n_select += 1
+    else:
+        n_notselect += 1
+
+print(f"SELECTED {n_select}, NOT SELECTED {n_notselect}")
