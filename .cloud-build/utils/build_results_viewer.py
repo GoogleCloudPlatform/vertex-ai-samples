20b384a4eb6a657f6e292ef912ad8f0c97ddccc3
Andrew Ferlitsch <aferlitsch@google.com>
2023-11-08 21:18:44 +0000

    fix: GCS file

--- .cloud-build/utils/build_results_viewer.py
+++ .cloud-build/utils/build_results_viewer.py
@@ -5,12 +5,20 @@ Cloud Storage location: gs://cloud-build-notebooks-presubmit/build_results/
 '''
 import argparse
 import json
+from  util import download_file
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', dest='file',
                     default='build.json', type=str, help='build results file')
 args = parser.parse_args()
+
+if args.file.startswith("gs://"):
+    path = args.file[5:]
+    bucket = path.split('/')[0]
+    file   = path[len(bucket)+1:]
+    download_file(bucket, file, "build.json")
+    args.file = "build.json"
 
 with open(args.file, 'r') as f:
     results = json.load(f)
